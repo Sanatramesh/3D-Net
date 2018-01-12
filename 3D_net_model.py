3782cc5a67ad307db79d9454fafcf2c9749cecd5
Sanatramesh <sanat.r@iiitb.org>
2018-01-12 14:56:30 +0530

    Project init

---
+++ 3D_net_model.py
@@ -0,0 +1,33 @@
+import os
+import sys
+import time
+import argparse
+import numpy as np
+import tensorflow as tf
+
+class TDNet:
+
+    def __init__(self):
+        pass
+
+    def build_model(self):
+        pass
+
+class ModelTraining:
+
+    def __init__(self):
+        pass
+
+    def train_model(self):
+        pass
+
+class ModelTesting:
+
+    def __init__(self):
+        pass
+
+    def test_model(self):
+        pass
+
+if __name__ == '__main__':
+    net = TDNet()

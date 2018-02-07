input scaling fixed

TD_net_model.py
@@ -306,11 +306,9 @@ class TDNet_VGG11(TDNet):
 
         # Multiply features
         #feat_combined = np.multiply( left_feat, right_feat )
-        #feat_combined = tf.multiply( left_feat, right_feat )
 
         # Add features
-        # feat_combined = np.add( left_feat, right_feat )
-        feat_combined = self.sess.run(tf.add( left_feat, right_feat ))
+        feat_combined = np.add( left_feat, right_feat )
 
         # Interweaving features
         # shp = list(left_feat.shape)
@@ -357,7 +355,7 @@ class ModelTraining:
                 train_data = read_data( data_shuffled[ batch:batch + 5 ] )
                 self.model.train_batch( train_data )
                 training_loss += self.model.compute_loss( train_data )
-
+                #print("Batch no. : ",batch)
             training_loss /= (self.no_data // self.batch_size)
 
             # validate the model and print test, validation accuracy

vgg16_encoder.py
@@ -30,10 +30,10 @@ class Vgg16:
 
         start_time = time.time()
         print("build model started")
-        rgb_scaled = rgb * 255.0
+        #rgb_scaled = rgb * 255.0
 
         # Convert RGB to BGR
-        red, green, blue = tf.split(value = rgb_scaled, num_or_size_splits=3, axis = 3)
+        red, green, blue = tf.split(value = rgb, num_or_size_splits=3, axis = 3)
         assert red.get_shape().as_list()[1:] == [HEIGHT, WIDTH, 1]
         assert green.get_shape().as_list()[1:] == [HEIGHT, WIDTH, 1]
         assert blue.get_shape().as_list()[1:] == [HEIGHT, WIDTH, 1]